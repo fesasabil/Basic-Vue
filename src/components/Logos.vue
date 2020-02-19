861c53cecf7dbee3c40069f2fe5a2082c10031f9
fesasabil <kijee212@gmail.com>
2020-02-19 22:03:59 +0700

    add_designAsset

--- src/components/Logos.vue
+++ src/components/Logos.vue
@@ -1,24 +1,28 @@
 <template>
     <div class="logos-wrapper">
-        <h2>Find Best Logo</h2>
+        <h2 class="title-header">Find Best Logo</h2>
         <div class="item-logos">
-            <img src="" alt="">
+            <img class="logo" alt="Anime logo" src="../assets/anime.png">
         </div>
 
         <div class="item-logos">
-            <img src="" alt="">
+            <img class="logo" alt="Anime logo" src="../assets/ann.png">
         </div>
 
         <div class="item-logos">
-            <img src="" alt="">
+            <img class="logo" alt="Anime logo" src="../assets/animm.png">
         </div>
 
         <div class="item-logos">
-            <img src="" alt="">
+            <img class="logo" alt="Anime logo" src="../assets/anime.png">
         </div>
 
         <div class="item-logos">
-            <img src="" alt="">
+            <img class="logo" alt="Anime logo" src="../assets/ann.png">
+        </div>
+
+        <div class="item-logos">
+            <img class="logo" alt="Anime logo" src="../assets/animm.png">
         </div>
     </div>
 </template>
@@ -30,18 +34,27 @@ export default {
 </script>
 
 <style scope>
+    .title-header {
+        margin-bottom: 80px;
+        text-align: left;
+    }
+    
     .logos-wrapper {
-        width: 70%;
+        width: 62%;
         overflow: hidden;
         margin: auto;
     }
 
     .item-logos {
         float: left;
-        margin-right: 20px;
-        margin-bottom: 20px;
+        margin-right: 70px;
+        margin-bottom: 70px;
         width: 150px;
         height: 150px;
-        background: seagreen;
+    }
+
+    .item-logos .logo {
+        width: 150px;
+        height: 150px;
     }
 </style>
